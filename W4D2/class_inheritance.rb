7beafa4ce3a5fc9b712e9d0340a66a860fcf5f69
Josh Kim-Biggs <joshgykim@gmail.com>
2021-07-27 14:29:36 -0700

    Completed Class Inheritance

--- W4D2/class_inheritance.rb
+++ W4D2/class_inheritance.rb
@@ -9,34 +9,57 @@ class Employee
     end
 
     def bonus(multiplier)
-        bonus = salary * multiplier
-
-        return bonus
+        salary * multiplier
     end
 
-    private
+    protected
 
-    attr_reader :salary
+    attr_accessor :boss
+    attr_accessor :salary
 end
 
 class Manager < Employee
 
-    def initialize(name, title, salary, boss, employees) 
-        super(name, title, salary, boss)
-        @employees = employees
+    attr_accessor :employees
+
+    def initialize(name, title, salary, boss) 
+        super
+        @employees = []
+    end
+
+    def add_employees(*employee)
+        self.employees += employee
+        employee.each { |person| person.boss = self }
     end
 
     def bonus(multiplier)
+        find_total_salary * multiplier
+    end
+
+    def find_total_salary
         total_salary = 0
         employees.each do |employee|
             total_salary += employee.salary
-            if !employee.employees.nil?
-                
-
-        
-
+            if employee.is_a?(Manager)
+                total_salary += employee.find_total_salary
+            end
+        end
+        total_salary
     end
-    # managers might report to higher level managers
+
 end
 
 
+ned = Manager.new("Ned", "Founder", 1000000, nil)
+darren = Manager.new("Darren", "TA Manager", 78000, nil)
+
+ned.add_employees(darren)
+
+shawna = Employee.new("Shawna", "TA", 12000, nil)
+david = Employee.new("David", "TA", 10000, nil)
+
+darren.add_employees(shawna, david)
+
+p ned.bonus(5)
+p darren.bonus(4)
+p david.bonus(3)
