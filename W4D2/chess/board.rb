64b6c272541faa22915da2b04c55ab998e6af7d4
JoeJoegb922 <josephlam922@gmail.com>
2021-07-27 16:33:40 -0700

    added piece slider stepper and pawn class

--- W4D2/chess/board.rb
+++ W4D2/chess/board.rb
@@ -9,10 +9,10 @@ class Board
     end
 
     def start_board
-        board = Array.new(8) { Array.new(8) { Piece.new } }
+        board = Array.new(8) { Array.new(8) { "P" } }
         board.each_with_index do |row, i|
             if i > 1 && i < 6
-                row.map! { |ele| ele = nil } 
+                row.map! { |ele| ele = "_" } 
             end
         end
         board
@@ -27,13 +27,27 @@ class Board
     end
 
     def move_piece(start_pos, end_pos)
-        if self[start_pos].nil?
+        if self[start_pos] == "_"
             raise "There is no piece here."
-        elsif !self[end_pos].nil?
+        elsif !self[end_pos] == "_"
             raise "The piece cannot be placed here."
         else
-            self[end_pos], self[start_pos] = self[start_pos], nil
+            self[end_pos], self[start_pos] = self[start_pos], "_"
         end
     end
 
+    def print
+        board.each do |row| 
+            p row.join(" ")
+        end
+    end
 end
+
+
+board = Board.new
+# p board 
+# board.move_piece([1,0], [3,0])
+# p board
+board.print
+board.move_piece([1,0], [3,0])
+board.print
--- W4D2/chess/piece.rb
+++ W4D2/chess/piece.rb
@@ -1,10 +1,66 @@
 class Piece
 
-    def initialize
+    def initialize(color, board, pos)
+        @color = color
+        @board = board
+        @pos = pos
+    end
 
+    def moves
+        case self.class 
+        when Sliders
+            self.move_dirs
+        end
     end
 
     def inspect
-        "piece"
+        "P"
+    end
+
+end
+
+module Slideable
+    HORIZONTAL_DIRS = [[0,1],[1,0],[-1,0],[0,-1]]
+    DIAGONAL_DIRS = [[1,1],[1,-1],[-1,1],[-1,-1]]
+    def horizontal_dirs 
+
+    end
+end
+
+class Sliders < Piece
+    include Slideable
+
+    def initialize(color, board, pos)
+        super
+    end
+
+    def symbol
+        @color
+    end
+
+    def move_dirs
+
+    end
+
+end
+
+class Steppers < Piece
+
+    def initialize(color, board, pos)
+        super
+    end
+end
+
+class Pawn < Piece
+
+    def initialize(color, board, pos)
+        super
+    end
+end
+
+class NullPiece < Piece
+
+    def initialize(color, board, pos)
+        super
     end
 end
