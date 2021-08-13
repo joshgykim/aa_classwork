7bab72dad65213633ba2de8ae09c1a7a13ae2d1c
Josh Kim-Biggs <joshgykim@gmail.com>
2021-08-13 15:45:03 -0700

    Finished new.html.erb

--- W6D5/nine_nine_dresses/app/controllers/cats_controller.rb
+++ W6D5/nine_nine_dresses/app/controllers/cats_controller.rb
@@ -14,4 +14,32 @@ class CatsController < ApplicationController
     end
   end
 
+  def new
+    @cat = Cat.new
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cat = Cat.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+  end
+
+  private
+  
+  def cat_params
+    params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
+  end
+
 end
