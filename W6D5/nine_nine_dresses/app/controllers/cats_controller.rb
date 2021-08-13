76e26a67e5f3373cea1d61fe567578110347c831
Vern Chao <vernchao91@gmail.com>
2021-08-13 14:25:12 -0700

    finished partials

---
+++ W6D5/nine_nine_dresses/app/controllers/cats_controller.rb
@@ -0,0 +1,17 @@
+class CatsController < ApplicationController
+
+  def index
+    @cats = Cat.all
+    render :index
+  end
+
+  def show
+    @cat = Cat.find_by(id: params[:id])
+    if @cat
+      render :show
+    else
+      render json: { error: "There is no cat by that ID"}, status: 404
+    end
+  end
+
+end
