5f8b1d63173c7f26f535c482e6fc562947256a4e
mrboovn <m9nguyen@ucsd.edu>
2021-08-18 14:09:53 -0700

    switch

--- W7D3/goal_app/app/models/user.rb
+++ W7D3/goal_app/app/models/user.rb
@@ -2,6 +2,36 @@ class User < ApplicationRecord
     validates :username, :session_token, uniqueness: true, presence: true
     validates :password, length: {minimum: 6}, allow_nil: true
     validates :password_digest, presence: true
-
+    attr_reader :password 
+    after_initialize :ensure_session_token
     # S P I R E
+    def self.find_by_credential(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            return user 
+        else
+            return nil 
+        end
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+
 end
--- W7D3/goal_app/spec/models/user_spec.rb
+++ W7D3/goal_app/spec/models/user_spec.rb
@@ -14,6 +14,27 @@ RSpec.describe User, type: :model do
   #Associations
 
   #Methods
+  describe "user find credentials" do 
+    it "find user with username and password" do 
+      minh = FactoryBot.create(:user, username: 'minh')
+      expect(User.find_by_credential("minh", "password")).to eq(minh) 
+    end
+
+    it "return nil if user does not exist" do 
+      expect(User.find_by_credential("random", "password")).to eq(nil)
+    end
+  end
+
+  describe "password= password" do 
+    it "create password_digest and set @password to password" do 
+      josh = FactoryBot.create(:user, password: 'helloworld', username: 'josh')
+      expect(josh.password_digest).not_to eq(nil)
+      expect(josh.password).to eq("helloworld")
+    end
+  end
+
+
+
 
 
 
