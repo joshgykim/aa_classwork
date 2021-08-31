e469eb841991219778453658e46777ea615902cf
Josh Kim-Biggs <joshgykim@gmail.com>
2021-08-31 14:59:50 -0700

    working on ttt

--- W9D2/tictactoe/src/index.js
+++ W9D2/tictactoe/src/index.js
@@ -1,6 +1,7 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view");
+const Game = require("../ttt_node/game");
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  const game = new Game();
+  const view = new View(game, document.getElementsByClassName('ttt')[0]);
 });
--- W9D2/tictactoe/src/ttt-view.js
+++ W9D2/tictactoe/src/ttt-view.js
@@ -1,11 +1,34 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.handler = this.handleClick.bind(this);
+    this.setupBoard();
+    
 
-  setupBoard() {}
+  }
+
+  setupBoard() {
+    const board = document.createElement('ul');
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const grid = document.createElement('li');
+        grid.dataset.pos = JSON.stringify([i,j]);
+        board.append(grid);
+      }
+    }
+    this.el.append(board);
+  }
   
   bindEvents() {}
 
-  handleClick(e) {}
+  handleClick(e) {
+    const el = e.target;
+    if (el.nodeName === "li") {
+      this.makeMove(el);
+    }
+  }
 
   makeMove(square) {}
 
