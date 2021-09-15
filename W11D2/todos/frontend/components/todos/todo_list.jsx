9fde91503295dfeed6a116f6e5c6a2e5003ea4af
Josh Kim-Biggs <joshgykim@gmail.com>
2021-09-15 14:33:08 -0700

    Working on phase 2

---
+++ W11D2/todos/frontend/components/todos/todo_list.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class TodoList extends React.Component {
+
+  render() {
+    return (
+      <h3>Todo List goes here!</h3>
+    )
+  }
+}
+
+export default TodoList
--- W11D2/todos/frontend/todo_redux.jsx
+++ W11D2/todos/frontend/todo_redux.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import configureStore from "./store/store"
-import {receiveTodos, receiveTodo, removeTodo} from "./actions/todo_actions"
-import {receiveSteps, receiveStep, removeStep} from "./actions/step_actions"
+import configureStore from "./store/store";
+import {receiveTodos, receiveTodo, removeTodo} from "./actions/todo_actions";
+import {receiveSteps, receiveStep, removeStep} from "./actions/step_actions";
+import App from "./components/app";
+import Root from "./components/Root";
+import { allTodos } from "./reducers/selectors";
+
 
 function Content() {
   return (
@@ -13,7 +17,22 @@ function Content() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const store = configureStore();
+  const seed = {
+    todos: {
+      1: {
+        id: 1,
+        body: 'learn selectors',
+        done: 'done'
+      },
+      2: {
+        id: 2,
+        body: 'look good doing it',
+        done: 'undone'
+      }
+    }
+  };
+
+  const store = configureStore(seed);
   window.receiveTodos = receiveTodos;
   window.receiveTodo = receiveTodo;
   window.removeTodo = removeTodo;
@@ -22,6 +41,8 @@ document.addEventListener("DOMContentLoaded", () => {
   window.removeStep = removeStep;
   window.store = store;
 
+  window.allTodos = allTodos;
+
   const content = document.getElementById("content");
-  ReactDOM.render(<Content/>, content);
+  ReactDOM.render(<Root store={store} />, content);
 })
