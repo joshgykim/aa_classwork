0e39ee9d19e597737c6799752247b7b6027f086e
Josh Kim-Biggs <joshgykim@gmail.com>
2021-09-14 17:28:39 -0700

    finished classwork

--- W11D2/todos/frontend/todo_redux.jsx
+++ W11D2/todos/frontend/todo_redux.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store"
 import {receiveTodos, receiveTodo, removeTodo} from "./actions/todo_actions"
+import {receiveSteps, receiveStep, removeStep} from "./actions/step_actions"
 
 function Content() {
   return (
@@ -16,6 +17,9 @@ document.addEventListener("DOMContentLoaded", () => {
   window.receiveTodos = receiveTodos;
   window.receiveTodo = receiveTodo;
   window.removeTodo = removeTodo;
+  window.receiveSteps = receiveSteps;
+  window.receiveStep = receiveStep;
+  window.removeStep = removeStep;
   window.store = store;
 
   const content = document.getElementById("content");
