e2d8f015a0b1937b65fe5efc950dc0be91e4a9da
Josh Kim-Biggs <joshgykim@gmail.com>
2021-09-16 17:26:55 -0700

    Started Phase 2

--- W11D2/todos/Todo_app/db/seeds.rb
+++ W11D2/todos/Todo_app/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+TodoItem.create([
+  { title: "TodoItem1", body: "TodoItem1Body", done: false },
+  { title: "TodoItem2", body: "TodoItem2Body", done: false },
+  { title: "TodoItem3", body: "TodoItem3Body", done: false },
+  { title: "TodoItem4", body: "TodoItem4Body", done: false },
+  { title: "TodoItem5", body: "TodoItem5Body", done: false },
+  { title: "TodoItem6", body: "TodoItem6Body", done: false },
+])
