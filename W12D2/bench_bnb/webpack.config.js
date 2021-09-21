5edba400d3eb033a6c9b5d490f79b2c1ec7a0bfc
Josh Kim-Biggs <joshgykim@gmail.com>
2021-09-21 14:39:30 -0700

    Finished User Model

--- W12D2/bench_bnb/webpack.config.js
+++ W12D2/bench_bnb/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/index.jsx',
+  entry: './frontend/bench_bnb.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
@@ -17,7 +17,7 @@ module.exports = {
         exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
-          query: {
+          options: {
             presets: ['@babel/env', '@babel/react']
           }
         },
