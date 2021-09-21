fb7c597f5b98588e497ac7a1ceb8d1ce4dec91e5
Mike Le <letumichael@gmail.com>
2021-09-21 15:15:24 -0700

    Updated backend routes and added show and create actions to users controller

---
+++ W12D2/bench_bnb/app/controllers/api/users_controller.rb
@@ -0,0 +1,20 @@
+class Api::UsersController < ApplicationController
+  def show
+    @user = User.find_by(id: params[:id])
+
+    if @user
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 404
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:password, :username)
+  end
+end
--- W12D2/bench_bnb/frontend/bench_bnb.jsx
+++ W12D2/bench_bnb/frontend/bench_bnb.jsx
@@ -5,3 +5,4 @@ document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
+
